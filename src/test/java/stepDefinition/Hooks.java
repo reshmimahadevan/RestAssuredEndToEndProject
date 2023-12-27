fa442bd133429c0ae4b7f4846cb5e50a1528b53f
Reshmi Mahadevan <reshmimahadevan04@gmail.com>
2023-12-26 20:28:58 -0800

    Included Hypen in address

--- src/test/java/stepDefinition/Hooks.java
+++ src/test/java/stepDefinition/Hooks.java
@@ -16,7 +16,7 @@ public class Hooks {
 		//place_id is static so calling directly with class name
 		if (stepDefinition.place_id == null) {
 
-			m.add_place_payload_with("Reshmi", "SFO-USA", "Tamil", "(+91) 923 588 2389 ",
+			m.add_place_payload_with("Reshmi", "SFO--USA", "Tamil", "(+91) 923 588 2389 ",
 					"https://rahulshettyacademy.com", 60, -29.467839, 15.865045);
 			m.user_calls_with_http_request("addPlaceAPI", "POST");
 			m.verify_place_Id_created_maps_to_using("Reshmi", "getPlaceAPI");
